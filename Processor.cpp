dd311e125d5e6a3415a62cb804fca66cd48fe6e1
Preesha Agrawal <cs5240888@cse.iitd.ac.in>
2026-04-05 20:59:19 +0530

    Processor.cpp made2

--- Processor.cpp
+++ Processor.cpp
@@ -14,3 +14,15 @@ Processor::Processor(ProcessorConfig& config){
     // Bitwise Logic
     // Load-Store Unit
 }
+
+void Processor::loadProgram(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file) {throw std::runtime_error("corrupted file");};
+    std::string line;
+    while (std::getline(file, line)) 
+    {
+        Instruction inst = lineToInst (line);
+        inst_memory.push_back(inst);
+    }
+
+}
